185c762f1d1fe009270b50cc4b5f1dfe39208f37
TakePan <44888546+TakeutiPanda@users.noreply.github.com>
2018-11-09 15:11:25 +0900

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,5 +1,28 @@
-class Room {
+class Room {	
 	public Room(){
 		
+		public void TutorialRoom(){ //チュートリア
+			
+		}
+		
+		public void EmptyRoom(){ //空部屋
+			
+		}
+		
+		public void EnemyRoom(){ //敵部屋
+		
+		}
+		
+		public void TreasureRoom{ //宝部屋
+		
+		}
+		
+		public void TrapRoom{ //罠部屋
+		
+		}
+		
+		public void GoolRoom{ //秘宝部屋
+		
+		}
 	}
 }
